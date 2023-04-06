aa338d57388a28a1633ea4163a45fb494aa2a55b
hoangvu03081 <hoangvu03081@gmail.com>
2023-04-06 22:40:28 +0700

    Add projects table

---
+++ db/migrate/20230406153116_add_projects_table.rb
@@ -0,0 +1,12 @@
+class AddProjectsTable < ActiveRecord::Migration[7.0]
+  def change
+    create_table :projects, id: :uuid do |t|
+      t.text :name
+      t.text :description
+
+      t.timestamps
+    end
+
+    add_reference :projects, :user, foreign_key: true, type: :uuid
+  end
+end
