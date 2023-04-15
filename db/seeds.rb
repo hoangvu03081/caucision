165ec04a7ffedc62f960b71bef7fa2b0d5fb75e8
Son Truong <sontruong@pixelcent.com>
2023-04-15 18:04:56 +0700

    Update redirect_uri

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@ Doorkeeper.config.application_model.create(
   name:   'Frontend App',
   uid:    Rails.application.credentials[:frontend_client_id],
   secret: Rails.application.credentials[:frontend_client_secret],
-  redirect_uri: 'http://localhost:3000',
+  redirect_uri: 'http://localhost:3000/auth-callback',
   confidential: false
 )
