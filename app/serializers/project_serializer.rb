2c5fa8b57a485cbfc97ac537ddd8d3d62f1bf448
Son Truong <sontruong@pixelcent.com>
2023-04-24 16:32:38 +0700

    Add data schema

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -2,7 +2,8 @@ class ProjectSerializer < ActiveModel::Serializer
   attributes :id, :name, :description,
              :created_at, :updated_at,
              :data_imported, :model_trained,
-             :control_promotion, :graph_imported
+             :control_promotion, :graph_imported,
+             :data_schema,
 
   def graph_imported
     !!object.causal_graph
