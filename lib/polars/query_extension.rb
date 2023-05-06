953bb302739f34ce8a961cb40e96df09f2e5f4b1
hoangvu03081 <hoangvu03081@gmail.com>
2023-05-06 21:07:23 +0700

    Update filter_by to coerce params dtype based on dataframe schema

--- lib/polars/query_extension.rb
+++ lib/polars/query_extension.rb
@@ -1,5 +1,19 @@
 module Polars
   module QueryExtension
+    TYPE_CONVERSION_MAPPINGS = {
+      Polars::Int8 => 'to_i',
+      Polars::Int16 => 'to_i',
+      Polars::Int32 => 'to_i',
+      Polars::Int64 => 'to_i',
+      Polars::UInt8 => 'to_i',
+      Polars::UInt16 => 'to_i',
+      Polars::UInt32 => 'to_i',
+      Polars::UInt64 => 'to_i',
+      Polars::Float32 => 'to_f',
+      Polars::Float64 => 'to_f',
+      Polars::Utf8 => 'to_s'
+    }.freeze
+
     class << self
       def query(dataframe, filter_by: nil, sort_by: nil)
         dataframe = filter(dataframe, filter_by:) if filter_by
@@ -27,6 +41,15 @@ module Polars
       end
 
       def filter(dataframe, filter_by:)
+        filter_by = filter_by.select { |column| dataframe.columns.include?(column) }
+
+        filter_by = filter_by.to_h do |column, value|
+          column_dtype = dataframe[column].dtype
+          conversion_method = TYPE_CONVERSION_MAPPINGS[column_dtype]
+
+          [column, conversion_method ? value.send(conversion_method) : value]
+        end
+
         conditions =
           filter_by
             .map { |column_name, value| (Polars.col(column_name) == value) }
