eab47809ad6c1d1e42b093aadd91c62af7da45b1
hoangvu03081 <hoangvu03081@gmail.com>
2023-07-13 05:38:46 +0700

    Update seeding by environment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,16 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
-Doorkeeper.config.application_model.create(
-  name:   'Frontend App',
-  uid:    Rails.application.credentials[:frontend_client_id],
-  secret: Rails.application.credentials[:frontend_client_secret],
-  redirect_uri: 'http://localhost:3000/auth-callback',
-  confidential: false
-)
+model = Doorkeeper.config.application_model
+
+record = model.find_by(name: 'Frontend App')
+
+if record
+  record.update(
+    name:   'Frontend App',
+    uid:    Rails.application.credentials[:frontend_client_id],
+    secret: Rails.application.credentials[:frontend_client_secret],
+    redirect_uri: ENV['CALLBACK_URL'],
+    confidential: false
+  )
+end
